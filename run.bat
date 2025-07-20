7231832ead1eaaba370ca4677a115dd3b19d5b93
Francinne Manatad <129661559+JOOSH1T@users.noreply.github.com>
2025-07-21 03:23:14 +0800

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 @echo off
 cd src
-javac -cp ".;../lib/mysql-connector-j-9.3.0.jar" *.java
-java -cp ".;../lib/mysql-connector-j-9.3.0.jar" Driver
+javac -cp "../lib/mysql-connector-j-9.3.0.jar;." *.java
+cd ..
+java -cp "lib/mysql-connector-j-9.3.0.jar;src" Driver
--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-# Change to the src directory
 cd src
-
-# Compile all Java files with the MySQL connector in the classpath
 javac -cp "../lib/mysql-connector-j-9.3.0.jar:." *.java
-
-# Run the Driver class with the MySQL connector in the classpath
-java -cp "../lib/mysql-connector-j-9.3.0.jar:." Driver
+cd ..
+java -cp "lib/mysql-connector-j-9.3.0.jar:src" Driver
